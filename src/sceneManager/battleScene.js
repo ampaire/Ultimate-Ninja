a1ff86c6c6a9966998af433c0deadf3aa8fa15b9
ampaire <femayapr@gmail.com>
2020-04-22 09:42:22 -0600

    Add the actions to the player menu

--- src/sceneManager/battleScene.js
+++ src/sceneManager/battleScene.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-plusplus */
 /* eslint-disable eqeqeq */
 /* eslint-disable no-multi-assign */
 /* eslint-disable no-unused-vars */
@@ -51,9 +52,136 @@ const PlayerCharacter = new Phaser.Class({
     },
 });
 
+const MenuItem = new Phaser.Class({
+  Extends: Phaser.GameObjects.Text,
+
+  initialize:
+
+    function MenuItem(x, y, text, scene) {
+      Phaser.GameObjects.Text.call(this, scene, x, y, text, { color: '#ffffff', align: 'left', fontSize: 15 });
+    },
+
+  select() {
+    this.setColor('#f8ff38');
+  },
+
+  deselect() {
+    this.setColor('#ffffff');
+  },
+
+});
+
+const Menu = new Phaser.Class({
+  Extends: Phaser.GameObjects.Container,
+
+  initialize:
+
+    function Menu(x, y, scene, heroes) {
+      Phaser.GameObjects.Container.call(this, scene, x, y);
+      this.menuItems = [];
+      this.menuItemIndex = 0;
+      this.heroes = heroes;
+      this.x = x;
+      this.y = y;
+    },
+  addMenuItem(unit) {
+    const menuItem = new MenuItem(0, this.menuItems.length * 20, unit, this.scene);
+    this.menuItems.push(menuItem);
+    this.add(menuItem);
+  },
+  moveSelectionUp() {
+    this.menuItems[this.menuItemIndex].deselect();
+    this.menuItemIndex--;
+    if (this.menuItemIndex < 0) this.menuItemIndex = this.menuItems.length - 1;
+    this.menuItems[this.menuItemIndex].select();
+  },
+  moveSelectionDown() {
+    this.menuItems[this.menuItemIndex].deselect();
+    this.menuItemIndex++;
+    if (this.menuItemIndex >= this.menuItems.length) { this.menuItemIndex = 0; }
+    this.menuItems[this.menuItemIndex].select();
+  },
+  select(index) {
+    if (!index) { index = 0; }
+    this.menuItems[this.menuItemIndex].deselect();
+    this.menuItemIndex = index;
+    this.menuItems[this.menuItemIndex].select();
+  },
+  deselect() {
+    this.menuItems[this.menuItemIndex].deselect();
+    this.menuItemIndex = 0;
+  },
+  confirm() {
+    // wen the player confirms his slection, do the action
+  },
+  clear() {
+    for (let i = 0; i < this.menuItems.length; i++) {
+      this.menuItems[i].destroy();
+    }
+    this.menuItems.length = 0;
+    this.menuItemIndex = 0;
+  },
+  remap(units) {
+    this.clear();
+    for (let i = 0; i < units.length; i++) {
+      const unit = units[i];
+      this.addMenuItem(unit.type);
+    }
+  },
+});
+
+const HeroesMenu = new Phaser.Class({
+  Extends: Menu,
+
+  initialize:
+
+    function HeroesMenu(x, y, scene) {
+      Menu.call(this, x, y, scene);
+    },
+
+  remapHeroes() {
+    const { heroes } = this.battleScene;
+    this.heroesMenu.remap(heroes);
+  },
+});
+
+const ActionsMenu = new Phaser.Class({
+  Extends: Menu,
+
+  initialize:
+
+    function ActionsMenu(x, y, scene) {
+      Menu.call(this, x, y, scene);
+      this.addMenuItem('Attack');
+    },
+  confirm() {
+    // do something when the player selects an action
+  },
+
+});
+
+const EnemiesMenu = new Phaser.Class({
+  Extends: Menu,
+
+  initialize:
+
+    function EnemiesMenu(x, y, scene) {
+      Menu.call(this, x, y, scene);
+    },
+  confirm() {
+    // do something when the player selects an enemy
+  },
+
+  remapEnemies() {
+    const { enemies } = this.battleScene;
+    this.enemiesMenu.remap(enemies);
+  },
+});
+
 function getRndInteger(min, max) {
   return Math.floor(Math.random() * (max - min)) + min;
 }
+
 
 export default class GameScene extends Phaser.Scene {
   constructor() {
@@ -64,7 +192,7 @@ export default class GameScene extends Phaser.Scene {
   preload() {
     this.load.image('background', 'assets/background.png');
     this.load.spritesheet('disciple', 'assets/disciple.png', {
-      frameWidth: 48,
+      frameWidth: 47,
       frameHeight: 53,
     });
     this.load.spritesheet('enemies', './assets/enemies.png', {
@@ -87,37 +215,42 @@ export default class GameScene extends Phaser.Scene {
     this.graphics.strokeRect(510, 420, 280, 150);
     this.graphics.fillRect(510, 420, 280, 150);
 
-    const warrior = new PlayerCharacter(this, config.width / 2 - 200, config.height / 2, 'disciple', 120, 100);
+
+    const warrior = new PlayerCharacter(this, config.width / 2 + 200, config.height / 2, 'disciple', 1, 'player', 20);
+    warrior.flipX = true;
     this.add.existing(warrior);
     this.heroes = [warrior];
 
     const randomNum = getRndInteger(1, 3);
 
     if (randomNum == 1) {
-      const mushroom = new Enemy(this, config.width / 2 + 200, config.height / 2, 'enemies', 0, 'Shroom', 20);
-      const ent = new Enemy(this, config.width / 2 + 200, config.height / 2, 'enemies', 1, 'Ent', 20);
-      const duck = new Enemy(this, config.width / 2 + 200, config.height / 2, 'enemies', 3, 'Mad Duck', 20);
-      const pig = new Enemy(this, config.width / 2 + 200, config.height / 2, 'enemies', 4, 'Magik Pig', 20);
-      const flower = new Enemy(this, config.width / 2 + 200, config.height / 2, 'enemies', 5, 'Flower', 20);
+      const mushroom = new Enemy(this, config.width / 2 - 200, config.height / 2, 'enemies', 0, 'Poisonous Shroom', 20);
+      const Ant = new Enemy(this, config.width / 2 - 200, config.height / 2, 'enemies', 1, 'Crazy Ant', 20);
+      const duck = new Enemy(this, config.width / 2 - 200, config.height / 2, 'enemies', 3, 'Mad Duck', 20);
+      const pig = new Enemy(this, config.width / 2 - 200, config.height / 2, 'enemies', 4, 'Serious Pig', 20);
+      const flower = new Enemy(this, config.width / 2 - 200, config.height / 2, 'enemies', 5, 'Magic Flower', 20);
 
-      const monster1 = [mushroom, ent, duck, pig, flower];
+      const monster1 = [mushroom, Ant, duck, pig, flower];
 
       const enemy1 = monster1[getRndInteger(0, monster1.length)];
+      enemy1.flipX = true;
       this.add.existing(enemy1);
       this.enemies = [enemy1];
 
       this.units = this.heroes.concat(this.enemies);
     } else if (randomNum == 2) {
-      const mushroom = new Enemy(this, config.width / 2 + 200, config.height / 2, 'enemies', 0, 'Shroom', 20);
-      const ent = new Enemy(this, config.width / 2 + 200, config.height / 2, 'enemies', 1, 'Ent', 20);
-      const duck = new Enemy(this, 210, 70, 'enemies', 3, 'Mad Duck', 20);
-      const pig = new Enemy(this, 210, 70, 'enemies', 4, 'Magik Pig', 20);
+      const mushroom = new Enemy(this, config.width / 2 - 200, config.height / 2, 'enemies', 0, 'Poisonous Shroom', 20);
+      const Ant = new Enemy(this, config.width / 2 - 200, config.height / 2, 'enemies', 1, 'Crazy Ant', 20);
+      const duck = new Enemy(this, 210, 180, 'enemies', 3, 'Mad Duck', 20);
+      const pig = new Enemy(this, 210, 180, 'enemies', 4, 'Serious Pig', 20);
 
-      const monster1 = [mushroom, ent];
+      const monster1 = [mushroom, Ant];
       const monster2 = [duck, pig];
 
       const enemy1 = monster1[getRndInteger(0, monster1.length)];
       const enemy2 = monster2[getRndInteger(0, monster2.length)];
+      enemy1.flipX = true;
+      enemy2.flipX = true;
 
       this.add.existing(enemy1);
       this.add.existing(enemy2);
@@ -126,19 +259,23 @@ export default class GameScene extends Phaser.Scene {
 
       this.units = this.heroes.concat(this.enemies);
     } else if (randomNum == 3) {
-      const mushroom = new Enemy(this, config.width / 2 + 200, config.height / 2, 'enemies', 0, 'Shroom', 20);
-      const ent = new Enemy(this, config.width / 2 + 200, config.height / 2, 'enemies', 1, 'Ent', 20);
-      const duck = new Enemy(this, 210, 70, 'enemies', 3, 'Mad Duck', 20);
-      const pig = new Enemy(this, 210, 70, 'enemies', 4, 'Magik Pig', 20);
-      const flower = new Enemy(this, 245, 70, 'enemies', 5, 'Flower', 20);
+      const mushroom = new Enemy(this, config.width / 2 - 200, config.height / 2, 'enemies', 0, 'Poisonous Shroom', 20);
+      const Ant = new Enemy(this, config.width / 2 - 200, config.height / 2, 'enemies', 1, 'Crazy Ant', 20);
+      const duck = new Enemy(this, 210, 200, 'enemies', 3, 'Mad Duck', 20);
+      const pig = new Enemy(this, 210, 200, 'enemies', 4, 'Serious Pig', 20);
+      const flower = new Enemy(this, 245, 190, 'enemies', 5, 'Magic Flower', 20);
 
-      const monster1 = [mushroom, ent];
+      const monster1 = [mushroom, Ant];
       const monster2 = [duck, pig];
       const monster3 = [flower];
 
       const enemy1 = monster1[getRndInteger(0, monster1.length)];
       const enemy2 = monster2[getRndInteger(0, monster2.length)];
       const enemy3 = monster3[getRndInteger(0, monster3.length)];
+
+      enemy1.flipX = true;
+      enemy2.flipX = true;
+      enemy3.flipX = true;
 
       this.add.existing(enemy1);
       this.add.existing(enemy2);
@@ -150,10 +287,31 @@ export default class GameScene extends Phaser.Scene {
     }
 
     this.index = -1;
+
+    this.menus = this.add.container();
+
+    this.heroesMenu = new HeroesMenu(520, 440, this);
+    this.actionsMenu = new ActionsMenu(320, 440, this);
+    this.enemiesMenu = new EnemiesMenu(20, 440, this);
+
+    this.currentMenu = this.actionsMenu;
+    this.heroesMenu.remap(this.heroes);
+    this.enemiesMenu.remap(this.enemies);
+
+    // this.remapHeroes();
+    // this.remapEnemies();
+
+    this.menus.add(this.heroesMenu);
+    this.menus.add(this.actionsMenu);
+    this.menus.add(this.enemiesMenu);
+
+
+    this.battleScene = this.scene.get('GameScene');
     gameOverText = this.add.text(400, 300, 'Game Over', {
       fontSize: '64px',
       fill: '#000',
     });
+
     gameOverText.setOrigin(0.5);
     gameOverText.setVisible(false);
 
